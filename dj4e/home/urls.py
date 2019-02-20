d15ba33111464e245da7eea6481601ddc9718b44
Sophia A. Simopoulos <sophsim@umich.edu>
2019-02-20 17:47:16 +0000

    Updating files for autos

--- dj4e/home/urls.py
+++ dj4e/home/urls.py
@@ -1,37 +1,8 @@
-"""locallibrary URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
-from . import views
 from django.views.generic import TemplateView
 
 # https://docs.djangoproject.com/en/2.1/topics/http/urls/
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='main.html'), name='main'),
+    path('', TemplateView.as_view(template_name='main_home.html'), name='main'),
 ]
-# New lines below to serve static files in debug mode
-import os
-from django.urls import re_path
-from django.views.static import serve
-from django.conf import settings
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^static/(?P<path>.*)$', serve, {
-            'document_root': os.path.join(BASE_DIR, 'home/static'),
-        }),
-    ]
--- dj4e/home/views.py
+++ dj4e/home/views.py
@@ -1,11 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    """View function for home page of site."""
-
-    # Generate counts of some of the main objects
-    hello_world = "hello world"
-
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'main.html', context=context)
